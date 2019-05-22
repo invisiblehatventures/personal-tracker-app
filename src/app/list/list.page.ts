afeed57f831e68e7e63d91622603c755e38a3ccb
Myk Klemme <mykklemme@gmail.com>
2019-05-21 22:08:08 -0700

    Hello world! 👏 👏 👏

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -21,10 +21,10 @@ export class ListPage implements OnInit {
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i < 21; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title: `Item ${i}`,
+        note: `#${i}`,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
